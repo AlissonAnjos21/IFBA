b24c8d9745a560fbb28857494a41d962ada22a7b
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-19 00:44:38 -0300

    Construtor Vazio

--- ExercicioBanco/src/Banco.java
+++ ExercicioBanco/src/Banco.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+
 public class Banco {
+    private String nome;
+    private int codigo;
+    private ArrayList clientes;
+    private ArrayList funcionarios;
+    private ArrayList contas;
+    private int agencias;
+
+    public Banco() {
+        this.nome = null;
+        this.codigo = 0;
+        this.clientes = null;
+        this.funcionarios = null;
+        this.contas = null;
+        this.agencias = 0;
+    }
+
+    public Banco(String nome, int codigo, ArrayList clientes, ArrayList funcionarios, ArrayList contas, int agencias) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.clientes = clientes;
+        this.funcionarios = funcionarios;
+        this.contas = contas;
+        this.agencias = agencias;
+
+    }
+
 }
