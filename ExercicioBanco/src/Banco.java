96e7a2cee2af4b91bba2bbac924e7c4ba0bd9b11
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-19 22:35:28 -0300

    Exercicio Banco

--- ExercicioBanco/src/Banco.java
+++ ExercicioBanco/src/Banco.java
@@ -3,28 +3,59 @@ import java.util.ArrayList;
 public class Banco {
     private String nome;
     private int codigo;
-    private ArrayList clientes;
-    private ArrayList funcionarios;
-    private ArrayList contas;
+    private ArrayList<Cliente> clientes = new ArrayList<>();
+    private ArrayList<Funcionario> funcionarios = new ArrayList<>();;
+    private ArrayList<Conta> contas = new ArrayList<>();;
     private int agencias;
 
     public Banco() {
-        this.nome = null;
-        this.codigo = 0;
-        this.clientes = null;
-        this.funcionarios = null;
-        this.contas = null;
-        this.agencias = 0;
+        this.nome = "";
     }
 
-    public Banco(String nome, int codigo, ArrayList clientes, ArrayList funcionarios, ArrayList contas, int agencias) {
+    public Banco(String nome, int codigo, Cliente cliente, Funcionario funcionario, Conta conta, int agencias) {
         this.nome = nome;
         this.codigo = codigo;
-        this.clientes = clientes;
-        this.funcionarios = funcionarios;
-        this.contas = contas;
+        this.addCliente(cliente);
+        this.addFuncionario(funcionario);
+        this.addConta(conta);
         this.agencias = agencias;
 
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void addCliente(Cliente cliente) {this.clientes.add(cliente);}
+
+    public void removeCliente(int id) {this.clientes.remove(this.clientes.stream().filter(cliente -> cliente.getId() == id).findFirst());}
+
+    public void addFuncionario(Funcionario funcionario) {this.funcionarios.add(funcionario);}
+
+    public void removeFuncionario(int id) {this.funcionarios.remove(this.funcionarios.stream().filter(funcionario -> funcionario.getId() == id).findFirst());}
+
+    public void addConta(Conta conta) {this.contas.add(conta);}
+
+    public void removeConta(int id) {this.contas.remove(this.contas.stream().filter(conta -> conta.getId() == id).findFirst());}
+
+    public void imprimeNomeClientes() {
+        clientes.forEach(cliente -> System.out.println(cliente.getNome()));
+    }
+
+    public Conta buscaConta(int numeroConta) throws Exception {
+        return this.contas.stream().filter(conta -> conta.getNumero() == numeroConta).findFirst().orElseThrow(() -> new Exception("Conta não existente"));
     }
 
 }
