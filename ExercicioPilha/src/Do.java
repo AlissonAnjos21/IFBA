3a6d4c69e8c0d805eeabada55e2a75ea86749e82
Alisson Natan dos Anjos Pinheiro <93294707+AlissonAnjos21@users.noreply.github.com>
2022-05-21 22:37:33 -0300

    Tentativa Valor Genérico

--- ExercicioPilha/src/Do.java
+++ ExercicioPilha/src/Do.java
@@ -1,2 +1,15 @@
-public class Do {
+public class Do<T> {
+
+    private T valor;
+    private Do item;
+
+    public Do() {
+        this.item = null;
+    }
+
+    public Do(T valor, Do item) {
+        this.valor = valor;
+        this.item = item;
+    }
+
 }
